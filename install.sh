9817bc60f3bc89a3a9c0e301fceeb49729870250
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2025-04-11 12:13:46 +1200

    Added install extension

--- install.sh
+++ install.sh
@@ -39,3 +39,44 @@ echo 'LS_COLORS+=$_ls_colors' >> ~/.zshrc
 # set gitconfig defaults
 git config --global push.autoSetupRemote true
 git config --global core.editor "code --wait"
+
+## Install extension
+
+
+# Visual Studio Code :: Package list
+pkglist=(
+ms-vscode.cpptools
+ms-python.python
+vscodevim.vim
+# kelvin.vscode-sshfs
+letmaik.git-tree-compare
+donjayamanne.githistory
+eamodio.gitlens
+# coenraads.bracket-pair-colorizer-2
+vscode-icons-team.vscode-icons
+equinusocio.vsc-material-theme-icons
+yzane.markdown-pdf
+# ics.japanese-proofreading
+# platformio.platformio-ide
+github.github-vscode-theme
+# bbrakenhoff.solarized-light-custom
+oderwat.indent-rainbow
+# streetsidesoftware.code-spell-checker
+christian-kohler.path-intellisense
+mhutchie.git-graph
+Gruntfuggly.todo-tree
+hediet.vscode-drawio
+github.vscode-pull-request-github
+marp-team.marp-vscode
+shd101wyy.markdown-preview-enhanced
+shuworks.vscode-table-formatter
+esbenp.prettier-vscode
+davidanson.vscode-markdownlint
+njpwerner.autodocstring
+ms-python.vscode-pylance
+ms-vscode-remote.remote-ssh
+)
+
+for i in ${pkglist[@]}; do
+  code --install-extension $i
+done
